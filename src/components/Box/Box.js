14371b2296b658c8c6eb689de69c1ba06aaddc21
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-27 15:51:12 -0800

    Game is fully functioning and the active state is set to false for all possible outcomes

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -14,6 +14,7 @@ export default function Box({ content, space }) {
   } = useContext(GameContext);
 
   function handleBoxClick() {
+    console.log('boardState', boardState);
     if (active === false) {
       return;
     } else if (content === 'X' || content === 'O') {
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -5,7 +5,7 @@ const GameContext = createContext();
 const GameProvider = ({ children }) => {
   const [currentPlayer, setCurrentPlayer] = useState('X');
   const [active, setActive] = useState(true);
-  const [gameMessage, setGameMessage] = useState("It's X's turn!");
+  const [gameMessage, setGameMessage] = useState(`It's ${currentPlayer}'s turn!`);
   const [boardState, setBoardState] = useState([
     { space: 1, content: '' },
     { space: 2, content: '' },
@@ -19,7 +19,7 @@ const GameProvider = ({ children }) => {
   ]);
 
   function gameConditionCheck() {
-    // CHECKS IF ALL SQUARES ARE FILLED
+    //Increments for every box filled
     let filledBoxes = 0;
     boardState.map((box) => {
       if (box.content !== '') {
@@ -29,13 +29,67 @@ const GameProvider = ({ children }) => {
 
     console.log('boardState[0].content', boardState[0].content === boardState[1].content);
     if (
-      boardState[0].content === boardState[1].content &&
-      boardState[0].content === boardState[2].content
+      //First Row Win
+      (boardState[0].content === 'X' &&
+        boardState[1].content === 'X' &&
+        boardState[2].content === 'X') ||
+      (boardState[0].content === 'O' &&
+        boardState[1].content === 'O' &&
+        boardState[2].content === 'O') ||
+      //Second Row Win
+      (boardState[3].content === 'X' &&
+        boardState[4].content === 'X' &&
+        boardState[5].content === 'X') ||
+      (boardState[3].content === 'O' &&
+        boardState[4].content === 'O' &&
+        boardState[5].content === 'O') ||
+      //Third Row Win
+      (boardState[6].content === 'X' &&
+        boardState[7].content === 'X' &&
+        boardState[8].content === 'X') ||
+      (boardState[6].content === 'O' &&
+        boardState[7].content === 'O' &&
+        boardState[8].content === 'O') ||
+      //First Column Win
+      (boardState[0].content === 'X' &&
+        boardState[3].content === 'X' &&
+        boardState[6].content === 'X') ||
+      (boardState[0].content === 'O' &&
+        boardState[3].content === 'O' &&
+        boardState[6].content === 'O') ||
+      //Second Column Win
+      (boardState[1].content === 'X' &&
+        boardState[4].content === 'X' &&
+        boardState[7].content === 'X') ||
+      (boardState[1].content === 'O' &&
+        boardState[4].content === 'O' &&
+        boardState[7].content === 'O') ||
+      //Third Column Win
+      (boardState[2].content === 'X' &&
+        boardState[5].content === 'X' &&
+        boardState[8].content === 'X') ||
+      (boardState[2].content === 'O' &&
+        boardState[5].content === 'O' &&
+        boardState[8].content === 'O') ||
+      //Left to Right diagonal
+      (boardState[0].content === 'X' &&
+        boardState[4].content === 'X' &&
+        boardState[8].content === 'X') ||
+      (boardState[0].content === 'O' &&
+        boardState[4].content === 'O' &&
+        boardState[8].content === 'O') ||
+      //Right to Left diagonal
+      (boardState[2].content === 'X' &&
+        boardState[4].content === 'X' &&
+        boardState[6].content === 'X') ||
+      (boardState[2].content === 'O' &&
+        boardState[4].content === 'O' &&
+        boardState[6].content === 'O')
     ) {
       setActive(false);
-      console.log('WOOHOO');
       setGameMessage(`Player ${currentPlayer} has won!!`);
     } else if (filledBoxes === 9) {
+      //Checks if all boxes are filled
       setActive(false);
       setGameMessage('Nobody won that sucks');
     }
